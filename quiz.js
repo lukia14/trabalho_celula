9ad3e7f3239712b7e455462c5972e2dd8d476166
Lucas Fernandes <lucas.silva.fernandes.lsf@gmail.com>
2024-10-25 10:22:49 -0300

    abacaxi

---
+++ quiz.js
@@ -0,0 +1,40 @@
+let nome = []
+let pontuacao = []
+let pontos = 0
+tabela = [nome,pontuacao]
+let i = 0
+while(true){
+    let aux = prompt('Qual o seu nome')
+    nome.push(aux)
+    let Q1 = prompt(`Para que serve a mitocondria? 
+    letra a) gerar ATP
+    letra b) fazer um pudim
+    letra c) para gerar oxigênio
+    letra d) guardar clorofila` )
+    if(Q1 == 'a'){
+        pontos++
+    }
+    let Q2 = prompt(`Qual é o nome da estrutura que envolve a célula vegetal e lhe dá suporte e proteção?? 
+    letra a) bolo de bolo
+    letra b) mitocondria
+    letra c) parede celular
+    letra d) lisossomo` )
+    if(Q2 == 'a'){
+        pontos++
+    }
+    while(true){
+        continuar = prompt('deseja continuar? s ou n')
+        if(continuar == 's' || continuar == 'n'){
+            break
+        }
+    }
+    if(continuar == 'n'){
+        break
+    }   
+    tabela[1][i] = pontos
+    i++
+}
+for(let j = 0;j<i;j++){
+   
+    document.getElementById('ranking').textContent =  document.getElementById('ranking').textContent +  `${tabela[0][j]} ${tabela[1][j]} pontos ; `
+}
